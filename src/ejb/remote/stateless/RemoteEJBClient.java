9878438aa6001928ddc64eb46b1278e916dfe783
tawnydev <kevin.matrix@hotmail.fr>
2016-10-18 12:45:41 +0200

    mise à jour
    invocation Stateful EJB

--- src/ejb/remote/stateless/RemoteEJBClient.java
+++ src/ejb/remote/stateless/RemoteEJBClient.java
@@ -1,15 +1,16 @@
+package ejb.remote.stateless;
 
-package org.jboss.as.quickstarts.ejb.remote.client;
- 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import java.util.Hashtable;
- 
-import org.jboss.as.quickstarts.ejb.remote.stateless.CalculatorBean;
-import org.jboss.as.quickstarts.ejb.remote.stateless.RemoteCalculator;
- 
+
+import ejb.remote.stateful.CounterBean;
+import ejb.remote.stateful.RemoteCounter;
+import ejb.remote.stateless.CalculatorBean;
+import ejb.remote.stateless.RemoteCalculator;
+
 /**
  * A sample program which acts a remote client for a EJB deployed on AS7 server.
  * This program shows how to lookup a stateless bean via JNDI and then invoke on it
@@ -21,6 +22,8 @@ public class RemoteEJBClient {
     public static void main(String[] args) throws Exception {    	
         // Invoke a stateless bean
     	invokeStatelessBean();
+    	invokeStatefulBean();
+    	invokeStatefulBean();
     }
  
     /**
@@ -53,18 +56,46 @@ public class RemoteEJBClient {
     }
  
     /**
-     * Looks up and returns the proxy to remote stateless calculator bean
+     * Looks up a stateful bean and invokes on it
      *
-     * @return
      * @throws NamingException
      */
-    private static RemoteCalculator lookupRemoteStatelessCalculator() throws NamingException {
-        final Hashtable jndiProperties = new Hashtable();
+    private static void invokeStatefulBean() throws NamingException {
+        // Let's lookup the remote stateful Counter
+    	final RemoteCounter statefulRemoteCounter = lookupRemoteStatefulCounter();
+        System.out.println("Obtained a remote stateful counter for invocation");
+        System.out.println("Nouvelle instance de counter n°"+statefulRemoteCounter.getNbInstance());
+        for (int i=0;i<5;i++){
+        	System.out.println("i="+i);
+        	System.out.println("index="+statefulRemoteCounter.getCount());
+        	statefulRemoteCounter.increment();
+        }
+        System.out.println("Dernière valeur de index="+statefulRemoteCounter.getCount());
+        for (int i=5; i>0; i--){
+        	System.out.println("i="+i);
+        	System.out.println("index="+statefulRemoteCounter.getCount());
+        	statefulRemoteCounter.decrement();
+        }
+        System.out.println("Dernière valeur de index="+statefulRemoteCounter.getCount());
+    }
+    
+    private static Hashtable getJNDI(){
+    	final Hashtable jndiProperties = new Hashtable();
         jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
         jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, org.jboss.naming.remote.client.InitialContextFactory.class.getName());
         jndiProperties.put(Context.PROVIDER_URL, "remote://localhost:4447");
         jndiProperties.put(Context.SECURITY_PRINCIPAL, "manager");
         jndiProperties.put(Context.SECURITY_CREDENTIALS, "tuyaux");
+        return jndiProperties;
+    }
+    /**
+     * Looks up and remotes stateless calculator bean
+     *
+     * @return
+     * @throws NamingException
+     */
+    private static RemoteCalculator lookupRemoteStatelessCalculator() throws NamingException {
+        final Hashtable jndiProperties = getJNDI();
         final Context context = new InitialContext(jndiProperties);
         final String moduleName = "EJBRemote";
         final String distinctName = "";
@@ -72,5 +103,22 @@ public class RemoteEJBClient {
         final String viewClassName = RemoteCalculator.class.getName();
         
         return (RemoteCalculator) context.lookup("ejb:/" + moduleName + "/" + beanName + "!" + viewClassName);
+    }
+    
+    /**
+     * Looks up and remotes stateful counter bean
+     *
+     * @return
+     * @throws NamingException
+     */
+    private static RemoteCounter lookupRemoteStatefulCounter() throws NamingException {
+    	final Hashtable jndiProperties = getJNDI();
+        final Context context = new InitialContext(jndiProperties);
+        final String moduleName = "EJBRemote";
+        final String distinctName = "";
+        final String beanName = CounterBean.class.getSimpleName();
+        final String viewClassName = RemoteCounter.class.getName();
+        
+        return (RemoteCounter) context.lookup("ejb:/" + moduleName + "/" + beanName + "!" + viewClassName +"?stateful");
     }
 }
